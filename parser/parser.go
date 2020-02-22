65ebfbe5f7f7ca88af6b593c4a7e0638070bcfc4
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2020-02-22 22:36:35 +0100

    Add the action parser method

--- parser/parser.go
+++ parser/parser.go
@@ -9,17 +9,29 @@ import (
 	"project-bootstrap/types"
 )
 
-// Parser parses stuff
+// Parser parses definitions
 type Parser struct{}
 
 // ParseWorkflow parses a workflow
 func (parser Parser) ParseWorkflow(handle io.Reader) (workflow types.Workflow, err error) {
-	workflow = types.Workflow{BaseDefinition: types.BaseDefinition{}}
+	workflow.BaseDefinition = types.BaseDefinition{}
 	content, err := ioutil.ReadAll(handle)
 	if err != nil {
 		return
 	}
 
 	err = yaml.Unmarshal(content, &workflow)
+	return
+}
+
+// ParseWorkflow parses a action
+func (parser Parser) ParseAction(handle io.Reader) (action types.Action, err error) {
+	action.BaseDefinition = types.BaseDefinition{}
+	content, err := ioutil.ReadAll(handle)
+	if err != nil {
+		return
+	}
+
+	err = yaml.Unmarshal(content, &action)
 	return
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -64,10 +64,59 @@ func TestParserParseWorkflowInvalidReader(t *testing.T) {
 }
 
 func TestParserParseWorkflowInvalidYaml(t *testing.T) {
-	workflowReader := loadFixture(t, "invalid_workflow.txt")
+	workflowReader := loadFixture(t, "invalid.txt")
 	parser := Parser{}
 
 	_, err := parser.ParseWorkflow(workflowReader)
+
+	assert.NotNil(t, err)
+
+	err = workflowReader.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestParserParseAction(t *testing.T) {
+	workflowReader := loadFixture(t, "test_action.yml")
+	assertion := assert.New(t)
+
+	parser := Parser{}
+	var (
+		workflow types.Action
+		err      error
+	)
+
+	workflow, err = parser.ParseAction(workflowReader)
+
+	assertion.Nil(err)
+	assertion.NotNil(workflow.BaseDefinition)
+	assertion.Equal("testid", workflow.ID)
+	assertion.Equal("testkind", workflow.Kind)
+	assertion.Equal("testname", workflow.MetaData.Name)
+	assertion.Equal("testversion", workflow.MetaData.Version)
+	assertion.Equal("testdescription", workflow.MetaData.Description)
+
+	err = workflowReader.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestParserParseActionInvalidReader(t *testing.T) {
+	invalidReader := InvalidReader{}
+
+	parser := Parser{}
+	_, err := parser.ParseAction(invalidReader)
+
+	assert.Equal(t, errInvalidReader, err)
+}
+
+func TestParserParseActionInvalidYaml(t *testing.T) {
+	workflowReader := loadFixture(t, "invalid.txt")
+	parser := Parser{}
+
+	_, err := parser.ParseAction(workflowReader)
 
 	assert.NotNil(t, err)
 
